01cc351c628e24f80074f5f2252c8cba574d2780
kyohiro <kyohiro@gmail.com>
2019-05-13 03:12:30 +0800

    Initial commit

---
+++ src/variables/general.jsx
@@ -0,0 +1,26 @@
+// ##############################
+// // // Tasks for TasksCard - see Dashboard view
+// #############################
+
+var bugs = [
+  'New hire training material - just updated 1 day ago',
+  "2019 regulatory rule changes - pending review",
+  "Equity risk data feed - new requirements - pending review",
+  "Global operational process change - just signed off 5 minutes ago"
+];
+var website = [
+  "Flooded: One year later, assessing what was lost and what was found when a ravaging rain swept through metro Detroit",
+  'Sign contract for "What are conference organizers afraid of?"'
+];
+var server = [
+  "Lines From Great Russian Literature? Or E-mails From My Boss?",
+  "Flooded: One year later, assessing what was lost and what was found when a ravaging rain swept through metro Detroit",
+  'Sign contract for "What are conference organizers afraid of?"'
+];
+
+module.exports = {
+  // these 3 are used to create the tasks lists in TasksCard - Dashboard view
+  bugs,
+  website,
+  server
+};
